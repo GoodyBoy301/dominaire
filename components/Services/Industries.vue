8ddecf371e22b24dfc1de347d8e04f233aa7485e
Goodness Urama <goodyboy301@gmail.com>
2025-08-12 19:57:23 +0100

    Add Industries component with styles and background

---
+++ components/Services/Industries.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="services-industries">
+    <figure class="services-industries-bg">
+      <img src="/images/industries-bg.webp" alt="" />
+    </figure>
+    <div class="services-industries-heading">
+      <h2>Industries</h2>
+      <p>
+        We've worked with a wide array of industries and always eager to explore
+        and innovate in new business domains.
+      </p>
+    </div>
+    <ul class="services-industries-list">
+      <li>fintech</li>
+      <hr />
+      <li>edtech</li>
+      <hr />
+      <li>real estate</li>
+      <hr />
+      <li>healthcare</li>
+    </ul>
+  </section>
+</template>
